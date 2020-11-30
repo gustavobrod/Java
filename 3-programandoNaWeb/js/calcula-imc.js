ee7ad2062d9d9edb3ec1b16a64d8c4a30b3b1530
gustavobrod <gustavo.brod@outlook.com>
2020-11-29 21:39:00 -0300

    java-29-11

---
+++ 3-programandoNaWeb/js/calcula-imc.js
@@ -0,0 +1,69 @@
+//console.log(document.querySelector("h1"));
+var titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//console.log(titulo.textContent);
+titulo.textContent = "Nutricionista";
+
+//IMC
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++)
+{
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+
+    if(!pesoEhValido){
+        console.log("Peso invalido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(!alturaEhValida){
+        console.log("Altura invalida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!"; 
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
+    }    
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if (altura >= 0 && altura <= 3.0){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function calculaImc(peso,altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+
+
