fb390f555aa8fb9e6f861bbfba8b678bdb888f01
gustavobrod <gustavo.brod@outlook.com>
2020-12-01 18:17:08 -0300

    java-01-12

--- 3-programandoNaWeb/js/form.js
+++ 3-programandoNaWeb/js/form.js
@@ -4,24 +4,25 @@ botaoAdicionar.addEventListener("click", function(){
     
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
-
-    var pacienteTr = montaTr(paciente);
-
+    
     var erros = validaPaciente(paciente);    
     if(erros.length > 0){
         exibeMensagensDeErro(erros);
         return;
     }
 
-    //Adiciona paciente na tabela
-    var tabela = document.querySelector("#tabela-pacientes");
-    
-    tabela.appendChild(pacienteTr);
-
+    adicionaPacienteNaTabela(paciente);
+            
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros){
     var ul = document.querySelector("#mensagens-erro");
