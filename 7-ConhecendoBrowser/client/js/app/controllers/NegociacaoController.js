e0bb7bd801a37e3814b7a4bdc1f911eeba8621b2
gustavobrod <gustavo.brod@outlook.com>
2021-02-06 19:39:00 -0300

    ConhecBrowPad_Mod3_aula9

--- 7-ConhecendoBrowser/client/js/app/controllers/NegociacaoController.js
+++ 7-ConhecendoBrowser/client/js/app/controllers/NegociacaoController.js
@@ -12,13 +12,18 @@ class NegociacaoController {
     adiciona(evento) {
         event.preventDefault();
         
-        let data = new Date(...
-            this._inputData.value
+        let data = new Date(
+            ...this._inputData.value
                 .split('-')
-                .map(function(item, indice) {
-                    return item - indice % 2
-                })
-            );
-        console.log(data);
-    }
+                .map((item, indice) =>  item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+    } 
 }
