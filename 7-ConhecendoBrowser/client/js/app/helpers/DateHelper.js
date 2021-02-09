6ba1936269a1f99a6a46678cce3929eb0a9cfab4
gustavobrod <gustavo.brod@outlook.com>
2021-02-09 00:01:18 -0300

    ConhecBrowPad_Mod4_aula4

--- 7-ConhecendoBrowser/client/js/app/helpers/DateHelper.js
+++ 7-ConhecendoBrowser/client/js/app/helpers/DateHelper.js
@@ -6,10 +6,13 @@ class DateHelper {
     }
     
     static dataParaTexto(data) {
-        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear(); 
+        
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
     
     static textoParaData(texto){
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw Error ('Formato correto é yyyy-mm-dd');
         return new Date(...texto.split('-').map((item, indice) =>  item - indice % 2));
     }
 }
