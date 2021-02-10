2af8380b7cf04c5599ece5283eb3e5e9f091a440
gustavobrod <gustavo.brod@outlook.com>
2021-02-09 22:55:13 -0300

    ConhecBrowPad_Mod5_aula2

--- 7-ConhecendoBrowser/client/js/app/controllers/NegociacaoController.js
+++ 7-ConhecendoBrowser/client/js/app/controllers/NegociacaoController.js
@@ -8,17 +8,15 @@ class NegociacaoController {
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
-        this._negociacoesView.update();
+        this._negociacoesView.update(this._listaNegociacoes);
     }
     
 
-    adiciona(evento) {
+    adiciona(event) {
         event.preventDefault();
-
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);        
         this._limaFormulario();
-        console.log(this._listaNegociacoes.negociacoes);
     }
 
     _criaNegociacao() {
@@ -32,8 +30,7 @@ class NegociacaoController {
     _limaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0;
-        
+        this._inputValor.value = 0.0;        
         this._inputData.focus();
     }
 }
