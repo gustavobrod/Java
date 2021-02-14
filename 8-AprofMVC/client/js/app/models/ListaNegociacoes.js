dc0821c3556b33ea2de70e6a3ebabe7166c9bc77
gustavobrod <gustavo.brod@outlook.com>
2021-02-14 16:18:51 -0300

    AprofMVC_Mod2_aula7

--- 8-AprofMVC/client/js/app/models/ListaNegociacoes.js
+++ 8-AprofMVC/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,14 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor() {
 
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
 
     adiciona(negociacao) {
         
-        this._negociacoes.push(negociacao);
-        this._armadilha(this);
+        this._negociacoes = [].concat(this._negociacoes, negociacao);
+        //this._negociacoes.push(negociacao);
     }
 
     get negociacoes() {
@@ -20,6 +19,5 @@ class ListaNegociacoes {
     esvazia() {
 
         this._negociacoes = [];
-        this._armadilha(this);
     }
 }
