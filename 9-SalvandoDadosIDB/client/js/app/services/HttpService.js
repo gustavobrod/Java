d3cc3dd7438a57fcf1699a6430198487055b6b3f
gustavobrod <gustavo.brod@outlook.com>
2021-03-02 21:08:37 -0300

    IndexDB_Mod5_aula2

--- 9-SalvandoDadosIDB/client/js/app/services/HttpService.js
+++ 9-SalvandoDadosIDB/client/js/app/services/HttpService.js
@@ -14,27 +14,12 @@ class HttpService {
     
     post(url, dado) {
 
+        return fetch(url, {
+            headers: {'Content-Type': 'application/json'},
+            method: 'POST',
+            body: JSON.stringify(dado)            
 
-            return new Promise((resolve, reject) => {
-
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", url, true);
-                xhr.setRequestHeader("Content-type", "application/json");
-                xhr.onreadystatechange = () => {
-
-                    if (xhr.readyState == 4) {
-
-                        if (xhr.status == 200) {
-
-                            resolve(JSON.parse(xhr.responseText));
-                        } else {
-
-                            reject(xhr.responseText);
-                        }
-                    }
-                };
-                xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-            });
-
-        }
+        })
+        .then(res => this._handleErrors(res));
+    }
 }
