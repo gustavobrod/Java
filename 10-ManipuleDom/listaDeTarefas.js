62fffa31e02bbec7ef5bfbd7511701b7efcb5ec8
gustavobrod <gustavo.brod@outlook.com>
2021-03-05 23:28:20 -0300

    ManDom-Mod2-Aula3

--- 10-ManipuleDom/listaDeTarefas.js
+++ 10-ManipuleDom/listaDeTarefas.js
@@ -2,9 +2,19 @@ const criarTarefa = () => {
     
     event.preventDefault();
 
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa);
     console.log(valor);
+
     input.value = "";
 }
 
