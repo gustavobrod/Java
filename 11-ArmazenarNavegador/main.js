c83a0f537dba5c0954f54582dc524ca719902182
gustavobrod <gustavo.brod@outlook.com>
2021-03-28 20:41:44 -0300

    ArmaNav2-Aula2

--- 11-ArmazenarNavegador/main.js
+++ 11-ArmazenarNavegador/main.js
@@ -21,6 +21,9 @@ const handleNovoItem = (event) => {
     const criaTarefa = criarTarefa(dados) 
         
     lista.appendChild(criaTarefa);
+
+    sessionStorage.setItem("tarefas", JSON.stringify(dados));
+
     input.value = " ";
 
 }
